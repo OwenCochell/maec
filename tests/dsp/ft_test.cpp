e3544bf5316693ca1e4aa8dea7d8112a49ba348d
Owen Cochell <owencochell@gmail.com>
2023-04-25 16:15:30 -0400

    Implemented some basic tests for DFT, not working as of now

--- tests/dsp/ft_test.cpp
+++ tests/dsp/ft_test.cpp
@@ -9,6 +9,70 @@
  * 
  */
 
+#include <random>
+#include <chrono>
+#include <vector>
 #include <gtest/gtest.h>
 
 #include "dsp/ft.hpp"
+
+TEST(DFT, Random) {
+
+    // Configure the signal size, should be even!
+
+    int size = 50;
+
+    // Create a collection of input data:
+
+    std::vector<long double> nums;
+
+    // Preallocate (just for fun)
+
+    nums.reserve(size);
+
+    // Create a random number generator:
+
+    std::default_random_engine rand;
+
+    // Iterate a number of times
+
+    for (int i = 0; i < size; ++i) {
+
+        // Add a random value to the collection:
+
+        nums.push_back(static_cast<long double>(static_cast<long double>(rand()) / rand.max()));
+    }
+
+    // Create containers for real and non-real parts
+
+    std::vector<long double> nonreal;
+    std::vector<long double> real;
+
+    // Reserve parts:
+
+    nonreal.reserve(size);
+    real.reserve(size);
+
+    // Create container for output signal:
+
+    std::vector<long double> output;
+
+    output.reserve(size);
+
+    // Now, send the data through the FT algorithm:
+
+    dft(nums.begin(), size, real.begin(), nonreal.begin());
+
+    // Send parts through inverse algorithm:
+
+    inv_dft(real.begin(), nonreal.begin(), size, output.begin());
+
+    // Ensure data matches:
+
+    for(int i = 0; i < size; ++i) {
+
+        // Check this value:
+
+        ASSERT_DOUBLE_EQ(nums.at(i), output.at(i));
+    }
+}
