b52bf34dd045360cd84b59a5d081f8dec7384a6c
Owen-Cochell <owencochell@gmail.com>
2023-04-28 14:56:11 -0400

    Minor changes, inverse DFT test does not work

--- README.md
+++ README.md
@@ -24,3 +24,28 @@ which provided a very simple yet powerful way to work with audio data.
 When completed, this project will contain modules for generating/playing back audio data,
 altering audio data (filters and whatnot), and outputting this data to multiple sources and backends.
 This project is geared towards realtime audio playback and analysis.
+
+## Current Features
+
+We currently have a lot of things implemented!
+As of now, most of this stuff is simply framework oriented,
+but as time goes on we will be shifting focus more and more onto the process
+of signal processing and audio synthesis.
+
+Here are some of our features:
+
+- A module framework!
+    - Fully polymorphic! It is trivial to create and integrate your modules into MAEC
+    - Modules can be linked and interact with each other
+    - A complete state chain, describing how the state of each module changes,
+        while allowing developers to hook custom actions into each state
+    - 
+- Home grown DSP algorithms!
+    - Various convolution implementations
+    - DFT and inverse DFT
+    - Hooks into more common, better algorithms created by others
+- A custom buffer for holding multi-channel signals!
+    - Custom iterators for accessing this data in a standardized way(also works with STD methods that work with iterators!)
+    - Provides different representations of multi-channel audio
+    - Tools for "squishing" data into something more workable
+- 
--- tests/dsp/ft_test.cpp
+++ tests/dsp/ft_test.cpp
@@ -10,17 +10,22 @@
  */
 
 #include <random>
-#include <chrono>
+#include <ctime>
 #include <vector>
 #include <gtest/gtest.h>
 
 #include "dsp/ft.hpp"
 
+/// Lots of data to compute
+std::vector<long double> ft_data = {-81.06114,-32.43124,-38.83822,-111.74227,52.93935,61.08610,-16.21751,-10.95117,-61.76994,-69.22861,0.42157,37.65178,80.44017,-2.04557,-85.95699,2.72509,-66.35543,-2.34489,-40.39149,-3.64988,-17.75498,71.05104,-48.02644,110.04518,-105.14626,-56.32757,95.84381,64.27214,-43.49700,-16.92379,-109.86381,84.79728,77.47628,57.24378,-110.99503,-55.18763,-95.60758,75.47220,-12.70082,-36.99770,-49.17589,-71.22563,108.82306,-110.42035,90.90029,-105.06521,45.91535,-1.05935,-92.97990,-44.71613};
+
+/// Real and non-real parts of above data
+std::vector<long double> reals = {-689.551,-360.762,-9.096,-327.623,-237.608,-56.482,-420.058,-405.181,410.474,384.976,-493.179,396.298,58.616,317.64,-125.924,-26.377,-186.769,192.097,-396.526,145.432,-187.555,-345.553,-606.114,-44.831,821.155,-357.606,821.155,-44.831,-606.114,-345.553,-187.555,145.432,-396.526,192.097,-186.769,-26.377,-125.924,317.64,58.616,396.298,-493.179,384.976,410.474,-405.181,-420.058,-56.482,-237.608,-327.623,-9.096,-360.762};
+std::vector<long double> nonreals = {0,-56.799,-88.46,114.599,59.815,-318.733,25.484,298.857,-52.783,639.085,-725.061,-217.793,-50.879,-81.235,-312.271,13.61,-225.7,267.968,-138.318,724.441,358.257,367.87,-379.416,-378.675,-204.907,0,204.907,378.675,379.416,-367.87,-358.257,-724.441,138.318,-267.968,225.7,-13.61,312.271,81.235,50.879,217.793,725.061,-639.085,52.783,-298.857,-25.484,318.733,-59.815,-114.599,88.46,56.799};
+
 /**
  * @brief Ensures the length functions are accurate
  * 
- * TODO: SEED RANDOM NUMBER GENERATOR!
- * Use time or something...
  */
 TEST(DFT, Length) {
 
@@ -31,11 +36,15 @@ TEST(DFT, Length) {
     ASSERT_EQ(length_ift(10), (10 - 1) * 2);
 }
 
+/**
+ * @brief Tests that random data can transformed and reversed correctly
+ * 
+ */
 TEST(DFT, Random) {
 
     // Configure the signal size, should be even!
 
-    int size = 4;
+    int size = 20;
 
     // Create a collection of input data:
 
@@ -48,6 +57,10 @@ TEST(DFT, Random) {
     // Create a random number generator:
 
     std::default_random_engine rand;
+
+    // Seed random number generator:
+
+    rand.seed(time(nullptr));
 
     // Create containers for real and non-real parts
 
@@ -71,7 +84,7 @@ TEST(DFT, Random) {
 
         // Add a random value to the collection:
 
-        nums.push_back(static_cast<long double>(static_cast<long double>(rand()) / std::default_random_engine::max()));
+        nums.push_back(static_cast<long double>((static_cast<long double>(rand()) - (std::default_random_engine::max() / 4.0)) / std::default_random_engine::max()));
 
         // Add a zero to the output signal:
 
@@ -101,5 +114,90 @@ TEST(DFT, Random) {
         // Check this value:
 
         ASSERT_NEAR(nums.at(i), output.at(i), 0.0000001);
+    }
+}
+
+/**
+ * @brief Tests that the DFT function can work correctly with known data
+ * 
+ */
+TEST(DFT, Known) {
+
+    // Create vectors containing responses:
+
+    std::vector<long double> real;
+    std::vector<long double> nonreal;
+
+    // Get length of output data:
+
+    int output_length = length_ft(ft_data.size());
+
+    // Reserve the vectors:
+
+    real.reserve(output_length);
+    nonreal.reserve(output_length);
+
+    // Fill the vectors with zeros:
+
+    for (int i = 0; i < output_length; ++i) {
+
+        real.push_back(0);
+        nonreal.push_back(0);
+    }
+
+    // Now, send the data through the DFT function:
+
+    dft(ft_data.begin(), ft_data.size(), real.begin(), nonreal.begin());
+
+    // Finally, ensure returned data is correct:
+
+    for (int i = 0; i < output_length; ++i) {
+
+        // Check real part:
+
+        ASSERT_NEAR(reals.at(i), real.at(i), 0.001);
+
+        // Check non-real part:
+
+        ASSERT_NEAR(nonreals.at(i), nonreal.at(i), 0.001);
+    }
+}
+
+/**
+ * @brief Tests that the inverse DFT works with known data
+ * 
+ */
+TEST(DFT, InvKnown) {
+
+    // Create vector for output data:
+
+    std::vector<long double> output;
+
+    // Determine size of output data:
+
+    int output_size = length_ift(nonreals.size());
+
+    // Reserve the output data:
+
+    output.reserve(output_size);
+
+    // Fill output buffer with 0:
+
+    for (int i = 0; i < output_size; ++i) {
+
+        output.push_back(0);
+    }
+
+    // Send data through function:
+
+    inv_dft(reals.begin(), nonreals.begin(), reals.size(), output.begin());
+
+    // Ensure outgoing data is correct:
+
+    for (int i = 0; i < output_size; ++i) {
+
+        // Ensure returned data is accurate:
+
+        ASSERT_NEAR(ft_data.at(i), output.at(i), 0.00001);
     }
 }
