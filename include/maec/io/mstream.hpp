cb9bc757ba35a7c37d8c3838ab951c10984dc788
Owen Cochell <owencochell@gmail.com>
2023-11-02 11:22:29 -0400

    More basic work on mstreams

--- include/maec/io/mstream.hpp
+++ include/maec/io/mstream.hpp
@@ -20,16 +20,180 @@
 #pragma once
 
 /**
- * @brief Base class for stream operations
+ * @brief Base class for mstreams
  * 
- * This class defines methods to be implemented by stream classes.
- * We provide methods for reading and writing data,
- * and some methods for determining if we are in read or write mode.
- * This class also lays out a basic state framework for determining
- * and setting the state of the stream.
+ * This class defines the basic components that should
+ * be present in ALL mstreams.
+ * We contain methods for state management, error checking,
+ * and configuration.
  * 
- * ALL mstream 
+ * mstreams have the following states:
+ * 
+ * - init - Inital state, no operations have been done
+ * - started - mstream has been started, and can be utilized
+ * - stopped - mstream is in stop state, no longer can be utilized
+ * - err - mstream is in a bad state, no longer can be utilized
  */
+template <typename C>
 class BaseMStream {
+private:
+
+    /// Boolean determining if this stream is input
+    bool input = false;
+
+    /// Boolean determining if this stream is output
+    bool output = false;
+
+    /// State definition
+    enum mstate {init, started, stoped, err};
+
+    /// Current state of mstream
+    mstate state = init;
+
+public:
+
+    /**
+     * @brief Start method for mstreams
+     *
+     * This method is called when the user is
+     * ready to utilize the mstream.
+     * We will not only configure the state of the mstream,
+     * but we will also call the child start method.
+     * Child mstreams should put any start code into this method.
+     * 
+     * Before a mstream is used, it SHOULD be started.
+     */
+    void start() {
+
+        // call child method:
+
+        static_cast<C*>(this)->start();
+
+        // Set state:
+
+        this->state = started;
+    }
+
+    /**
+     * @brief Stop method for mstreams
+     * 
+     * This method is called when the user is
+     * ready to stop the mstream.
+     * We will configure the state and call the child stop method.
+     * Child streams should put any stop code into this method.
+     * 
+     * After a stream has been stopped, it SHOULD NOT
+     * be used again.
+     * If an mstream is in the stop state,
+     * then the state should be considered useless.
+     */
+    void stop() {
+
+        // Call child method:
+
+        static_cast<C*>(this)->stop();
+
+        // Set stop state:
+
+        this->state = stoped;
+    }
+
+    /**
+     * @brief Gets the current state of the mstream
+     * 
+     * Te state of the mstream can be utilize
+     * to understand the current status of the mstream.
+     * 
+     * @return Current state of mstream
+     */
+    mstate get_state() const { return this->state; }
+
+    bool is_input() { return this->reading; }
+
+    /**
+     * @brief Determines if we are an output mstream
+     * 
+     * @return true We are output stream
+     * @return false We are not an output stream
+     */
+    bool is_output() { return this->output; }
+};
+
+/**
+ * @brief Base class for input mstreams
+ * 
+ * This class provides methods and functionality
+ * for input mstreams.
+ * We provide methods for reading data.
+ * 
+ * ALL mistreams MUST implement the methods and functionality defined here.
+ */
+template <typename C>
+class BaseMIStream : public BaseMIStream<BaseMIStream<C>> {
+public:
+
+    /**
+     * @brief Reads a number of bytes from the stream
+     * 
+     * This function handles the process of reading bytes from the stream.
+     * How this function operates is specific to the stream class,
+     * but the general idea is you provide a character array and a number 
+     * of bytes to read, and the component will read and place those
+     * bytes into the character array.
+     * 
+     * @param byts Charater array to place data into
+     * @param num Number of bytes to read
+     */
+    void read(char* byts, int num) {
+
+        // Call child method:
+
+        return static_cast<C*>(this)->read(byts, num);
+    }
+};
+
+/**
+ * @brief Base class for output mstreams
+ * 
+ * This class provides methods and functionality for output streams.
+ * We provide methods for writing data.
+ * 
+ * ALL mostreams MUST implement the methods and functionally defined here.
+ */
+template <typename C>
+class BaseMOStream : public BaseMStream<C> {
+public:
+
+    /**
+     * @brief Writes a number of bytes to the stream
+     * 
+     * This function handles the process of writing events to a stream.
+     * How this function operations is specific to the stream class,
+     * but the general idea is you provide a character array and a number
+     * of bytes to read, and the component will read and place those
+     * bytes into the character array.
+     * 
+     * @param byts Character array to write to stream
+     * @param num Number of bytes to write
+     */
+    void write(char* byts, int num) {
+
+        // Call child method:
+
+        return static_cast<C*>(this)->write(byts, num);
+    }
+};
+
+/**
+ * @brief Base class for IO streams
+ * 
+ * This class provides methods and functionality for input/output streams.
+ * We provide methods for reading and writing data.
+ * 
+ * ALL miostreams MUST inherit this class!
+ * 
+ */
+template <typename C>
+class BaseMIOStream : public BaseMIStream<BaseMIOStream<C>>, public BaseMOStream<BaseMIOStream<C>> {
 
 };
