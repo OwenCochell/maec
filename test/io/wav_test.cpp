fcd13d71c77babd08012e6e47d2fea980d51955f
Owen-Cochell <owencochell@gmail.com>
2023-12-27 19:08:41 -0500

    Added some minor tests for 8bit wave files

--- test/io/wav_test.cpp
+++ test/io/wav_test.cpp
@@ -15,7 +15,6 @@
 #include "io/mstream.hpp"
 
 // Define some wave data to be read:
-// Standard wave file
 // Wave file that is shorter than reported
 // Wave file with non-aligned data chunks with interruptions
 // Wave file with junk data at the end
@@ -30,7 +29,7 @@
  * 
  * This data has the following properties:
  * Channels = 2
- * Sample Rate = 44100
+ * Sample Rate = 48000
  * Bits Per Sample = 16
  * Size = 56
  * Number of frames = 5
@@ -39,6 +38,31 @@
 CharIStream wavs = {
     0x52, 0x49, 0x46, 0x46, 0x38, 0, 0, 0, 0x57, 0x41, 0x56, 0x45,  // WAVE header
     0x66, 0x6D, 0x74, 0x20, 0x10, 0, 0, 0, 0x01, 0, 0x02, 0, 0x80, 0xbb, 0, 0, 0, 0xee, 0x02, 0x00, 0x04, 0x00, 0x10, 0x00,  // FORMAT chunk
+    0x64, 0x61, 0x74, 0x61, 0x14, 0x00, 0x00, 0x00, // DATA header
+    0x85, 0xff, 0x9d, 0xff, // Data (till end of array)
+    0x42, 0xff, 0x9b, 0xff,
+    0x72, 0xff, 0x7d, 0xff,
+    0xe0, 0xff, 0x07, 0xff,
+    0x25, 0x00, 0xea, 0xfe,
+};
+
+/**
+ * @brief Wave file with different number of bits per sample
+ * 
+ * This wave file utilizes unsigned chars, and thus has 8 bits per sample.
+ * This is to ensure we can read files with differing formats.
+ * 
+ * This data has the following properties:
+ * Channels = 2
+ * Sample Rate = 48000
+ * Bits Per Sample = 8
+ * Size = 56
+ * Number of frames = 10
+ * 
+ */
+CharIStream wavb = {
+    0x52, 0x49, 0x46, 0x46, 0x38, 0, 0, 0, 0x57, 0x41, 0x56, 0x45,  // WAVE header
+    0x66, 0x6D, 0x74, 0x20, 0x10, 0, 0, 0, 0x01, 0, 0x02, 0, 0x80, 0xbb, 0, 0, 0, 0x77, 0x01, 0x00, 0x02, 0x00, 0x08, 0x00,  // FORMAT chunk
     0x64, 0x61, 0x74, 0x61, 0x14, 0x00, 0x00, 0x00, // DATA header
     0x85, 0xff, 0x9d, 0xff, // Data (till end of array)
     0x42, 0xff, 0x9b, 0xff,
@@ -57,7 +81,7 @@ CharIStream wavs = {
  * 
  * The data has the following properties:
  * Channels = 2
- * Sample Rate = 44100
+ * Sample Rate = 48000
  * Bits Per Sample = 16
  * Size = 84
  * Number of frames = 5
@@ -82,6 +106,10 @@ CharIStream jwavs = {
 
 
 std::array<int16_t, 10> data_wavs = {-123, -99, -190, -101, -142, -131, -32, -249, 37, -278};
+
+std::array<unsigned char, 20> data_wavb = {0x85, 0xff, 0x9d, 0xff, 0x42, 0xff, 0x9b,
+                                 0xff, 0x72, 0xff, 0x7d, 0xff, 0xe0, 0xff,
+                                 0x07, 0xff, 0x25, 0x00, 0xea, 0xfe};
 
 TEST_CASE("Base Wave", "[io][wav]") {
 
@@ -224,6 +252,72 @@ TEST_CASE("Wave Reader", "[io][wav]") {
             REQUIRE(data->at(1) == 0);
         }
     }
+
+    SECTION("8bit Wave Data", "Ensures wave files with 8 bits per sample can be read") {
+
+        // Create copy of standard wave:
+
+        auto swave = wavb;
+
+        // Attach mstream to wave file"
+
+        wav.set_stream(&swave);
+
+        // Start the wave file:
+
+        wav.start();
+
+        SECTION("Format Decoding", "Ensures the format data has been properly decoded") {
+
+            REQUIRE(wav.get_bits_per_sample() == 8);
+            REQUIRE(wav.get_blockalign() == 2);
+            REQUIRE(wav.get_byterate() == 96000);
+            REQUIRE(wav.get_bytes_per_sample() == 1);
+            REQUIRE(wav.get_channels() == 2);
+            REQUIRE(wav.get_format() == 1);
+            REQUIRE(wav.get_samplerate() == 48000);
+            REQUIRE(wav.get_size() == 56+8);
+        }
+
+        SECTION("Data Extraction", "Ensures we can extract audio data") {
+
+            // Set the buffer size to something small (just one frame):
+
+            wav.set_buffer_size(1);
+
+            // The wave data has 10 frames, so read 10 times:
+
+            for (int i = 0; i < 10; ++i) {
+
+                // Grab data:
+                auto data = wav.get_data();
+
+                // Ensure size is what we expect:
+
+                REQUIRE(data->channels() == 2);
+                REQUIRE(data->channel_capacity() == 1);
+
+                // Ensure contents are as we expect:
+
+                REQUIRE(uchar_mf(data_wavb[i*2]) == data->at(0));
+                REQUIRE(uchar_mf(data_wavb[i*2+1]) == data->at(1));
+            }
+
+            // We should be done with the file:
+
+            REQUIRE(wav.done());
+
+            // Just for fun, try to read once more:
+
+            auto data = wav.get_data();
+
+            // Should just be zeros:
+
+            REQUIRE(data->at(0) == 0);
+            REQUIRE(data->at(1) == 0);
+        }
+    }
+
 
     SECTION("Junk Wave", "Ensures we ignore junk data at end of file") {
 
