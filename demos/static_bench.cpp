3bb89453df9ed6563db45d108463a5dce84f33cf
OwenCochell <owencochell@gmail.com>
2025-11-18 00:45:53 -0600

    Same as dynamic benchmark

--- demos/static_bench.cpp
+++ demos/static_bench.cpp
@@ -29,13 +29,13 @@
 int main() {
 
     // Number of iterations to preform
-    const std::size_t iter = 50000;
+    const std::size_t iter = 100;
+
+    /// Number of processing iterations to preform
+    const std::size_t piter = 50;
 
     // Buffer size
-    const std::size_t buff = 10;
-
-    // Number of modules in each chain
-    const std::size_t nums = 100;
+    const std::size_t buff = 100;
 
     std::cout << "+================================+" << '\n';
     std::cout << " !Benchmarking chain performance!" << '\n';
@@ -100,7 +100,9 @@ int main() {
 
         // Process the chain
 
-        sink.meta_process();
+        for (int j = 0; j < piter; ++j) {
+            sink.meta_process();
+        }
 
         // Stop the clock
 
