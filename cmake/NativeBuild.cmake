04f707cdcc73fe1f4164267b5c2a50bd5d9356f6
OwenCochell <owencochell@gmail.com>
2025-12-09 13:52:58 -0600

    Renamed option, moved to main file

--- cmake/NativeBuild.cmake
+++ cmake/NativeBuild.cmake
@@ -1,12 +1,12 @@
-# Function to enable native CPU build for a target
+# Tools to enable and query native CPU optimizations
 
-option(MAEC_NATIVE_BUILD "Enables native CPU optimizations" ON)
-
+# Determine if the compiler supports -march=native
 include(CheckCXXCompilerFlag)
 CHECK_CXX_COMPILER_FLAG("-march=native" COMPILER_SUPPORTS_MARCH_NATIVE)
 
+# Function to enable native optimizations for a given target
 function(enable_native target_name)
-    if (COMPILER_SUPPORTS_MARCH_NATIVE AND MAEC_NATIVE_BUILD)
+    if (COMPILER_SUPPORTS_MARCH_NATIVE AND MAEC_NATIVE)
         if (CMAKE_CONFIGURATION_TYPES)
             target_compile_options(${target_name} PRIVATE
                 $<$<CONFIG:Release>:-march=native>
